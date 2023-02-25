a8cb62c9af59c311520a7cb4435f3b4f8aec4753
IliaPr <ilya.prianichnikov@gmail.com>
2023-02-25 12:18:09 +0700

    task2 done: added pop method

--- tests/test_func.py
+++ tests/test_func.py
@@ -13,6 +13,18 @@ class Test_func(unittest.TestCase):
         self.assertEqual(stack.push('data3'), print(stack.top.data))
         self.assertEqual(stack.push(''), None)
 
+    def test_stack_pop(self):
+        stack = Stack()
+        stack.push('data1')
+        data = stack.pop()
+        self.assertEqual(print(stack.top), None)
+
+    def test_stack_pop_2(self):
+        stack = Stack()
+        stack.push('data1')
+        stack.push('data2')
+        self.assertEqual(stack.pop(), 'data2')
+        self.assertEqual(stack.pop(), 'data1')
 
 if __name__ == '__main__':
     unittest.main()
